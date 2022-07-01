55527711401edc1317b85e0495a108b164a462f4
Shinenju1 <cosminarama97@gmail.com>
2022-07-01 23:45:54 +0200

    completato js vanilla e classi

--- 1 - Dice Game/main.js
+++ 1 - Dice Game/main.js
@@ -50,5 +50,48 @@ document.querySelector('.btn-hold').addEventListener("click",function()
 
 function nextPlayer()
 {
-    active
+    activePlayer === 0 ?activePlayer =1:activePlayer = 0; //
+    roundScore = 0;
+    var current0 = document.getElementById('current-0')
+    var current1 = document.getElementById('current-1')
+    current0.textContent='0';
+    current1.textContent='1';
+    // Cambia classe active in sequenza ogni volta che la function viene chiamata
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display='none'; // Fa scomparire il dado se tiri 1
+}
+
+document.querySelector(".btn-new").addEventListener("click",init);
+function init()
+{
+    scores = [0,0];
+    roundScore =0;
+    activePlayer=0;//0 is the first player , 1 is the second player
+
+    gamePlaying=true;
+
+var current0 = document.getElementById('current-0');
+var current1 = document.getElementById('current-1');
+current0.textContent='0';
+current1.textContent='0';
+document.getElementById('score-0').textContent=0;
+document.getElementById('score-1').textContent=0;
+document.querySelector('#name-0').textContent='Player 1';
+document.querySelector('#name-1').textContent='Player 2';
+
+//Changing the css-so the dice is not displayed initially
+document.querySelector('.dice').style.display='none';
+
+//Removing the winner and active class
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+
+//Making the first player as the active player
+document.querySelector('.player-0-panel').classList.add('active');
+
+
 }
