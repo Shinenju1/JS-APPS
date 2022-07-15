4126f1dad4cf2a10515fd5bcdf84baac59d53dcb
Shinenju1 <cosminarama97@gmail.com>
2022-07-15 14:25:29 +0200

    added variables to js

---
+++ 2 - Timer/app.js
@@ -0,0 +1,14 @@
+const timeDisplay = documen.querySelector('#timeDisplay')
+const startBtn = documen.querySelector('#startBtn')
+const pauseBtn = documen.querySelector('#pauseBtn')
+const resetBtn = documen.querySelector('#resetBtn')
+
+let startTime = 0;
+let elapsedTime = 0;let currentTime =0;
+let currentTime = 0;
+let paused = true;
+let intervalID;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+
