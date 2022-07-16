f61c33d0aac3b0a45572c07a0c14b72852df5909
Shinenju1 <cosminarama97@gmail.com>
2022-07-16 18:36:15 +0200

    fixed bugs and added game winning condition

--- 4 - Tic Tac Toe Simple Game/main.js
+++ 4 - Tic Tac Toe Simple Game/main.js
@@ -36,7 +36,38 @@ function updateCell(cell, index){
     cell.textContent = currentPlayer;
 
 }
+function changePlayer(){
+    currentPlayer = (currentPlayer =="X") ? "O": "X";
+    statusText.textContent = `${currentPlayer}'s turn`
+}
 function checkWinner(){
+    let roundWon = false;
+    for(let i = 0; < winConditions.length; i){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+
+        }
+        if(roundWon){
+            statusText.textContent = `${currentPlayer} wins!`;
+            running= false;
+        }
+        else if(!options.includes("")){
+            statusText.textContent = "Draw!";
+        }
+        else{
+            changePlayer();
+        }
+
+    }
 
 }
 function restartGame(){
