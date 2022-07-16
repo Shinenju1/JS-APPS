13ea1f2d3d10a60d700f72ea230c3fab9d537f6b
Shinenju1 <cosminarama97@gmail.com>
2022-07-16 16:40:02 +0200

    fixed js

--- 3 - Jajanken/main.js
+++ 3 - Jajanken/main.js
@@ -1,7 +1,7 @@
 const playerText = document.querySelector('#playerText');
 const computerText = document.querySelector('#computerText');
 const resultText = document.querySelector('#resultText');
-const choiceBtn = document.querySelectorAll('#choiceBtn');
+const choiceBtns = document.querySelectorAll('#choiceBtn');
 
 let player;
 let computer;
