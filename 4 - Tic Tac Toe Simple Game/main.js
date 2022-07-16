6ba871e594de3681589765e0eea6faa8a1bab54a
Shinenju1 <cosminarama97@gmail.com>
2022-07-16 18:40:20 +0200

    was missing an i in let i win conditions

--- 4 - Tic Tac Toe Simple Game/main.js
+++ 4 - Tic Tac Toe Simple Game/main.js
@@ -14,22 +14,23 @@ const winConditions = [
 let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let running = false;
+
 initializeGame();
 
-function initializeGame() {
+function initializeGame(){
     cells.forEach(cell => cell.addEventListener('click', cellClicked));
-    restartBtn.addEventListener('click', restartGame());
+    restartBtn.addEventListener('click', restartGame);
     running= true;
 }
 
 function cellClicked() {
     const cellIndex = this.getAttribute('cellIndex');
+
     if (options[cellIndex] != "" || !running){
         return;
     }
     updateCell(this,cellIndex);
     checkWinner();
-
 }
 function updateCell(cell, index){
     options[index] = currentPlayer;
@@ -37,12 +38,13 @@ function updateCell(cell, index){
 
 }
 function changePlayer(){
-    currentPlayer = (currentPlayer =="X") ? "O": "X";
+    currentPlayer = (currentPlayer == "X") ? "O": "X";
     statusText.textContent = `${currentPlayer}'s turn`
 }
 function checkWinner(){
     let roundWon = false;
-    for(let i = 0; < winConditions.length; i){
+
+    for(let i = 0; i < winConditions.length; i++){
         const condition = winConditions[i];
         const cellA = options[condition[0]];
         const cellB = options[condition[1]];
